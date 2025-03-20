d72bfee386de25839fd19e0385359de91f85d7d6
230221 <230221@LTI4305>
2025-03-20 16:12:46 -0300

    ok

--- Pam2/Pam2/ListaProdutoPage.xaml.cs
+++ Pam2/Pam2/ListaProdutoPage.xaml.cs
@@ -7,7 +7,13 @@ public partial class ListaProdutoPage : ContentPage
         List<Produto> lista = Produto.Lista;
 
         InitializeComponent();
-		lstProduto.ItemsSource = lista;
+		lstProduto.ItemsSource = lista; // update na lista da pagina
 
 	}
+
+    private void ViewCell_Tapped(object sender, EventArgs e)
+    {
+        
+        Navigation.PushAsync(new ProdutoPage() { BindingContext = (((ViewCell)sender).BindingContext as Produto) }); // fala q o sender deve ser lido como um viewcell, afinal sender é um nome generico
+    }
 }
