8cdd9631908aba237ac455d7357cc22405be1b74
230221 <230221@LTI4305>
2025-03-20 15:30:11 -0300

    funciona

--- Pam2/Pam2/ListaProdutoPage.xaml.cs
+++ Pam2/Pam2/ListaProdutoPage.xaml.cs
@@ -4,6 +4,10 @@ public partial class ListaProdutoPage : ContentPage
 {
 	public ListaProdutoPage()
 	{
-		InitializeComponent();
+        List<Produto> lista = Produto.Lista;
+
+        InitializeComponent();
+		lstProduto.ItemsSource = lista;
+
 	}
 }
--- Pam2/Pam2/ProdutoPage.xaml.cs
+++ Pam2/Pam2/ProdutoPage.xaml.cs
@@ -4,6 +4,6 @@ public partial class ProdutoPage : ContentPage
 {
 	public ProdutoPage()
 	{
-		InitializeComponent();
+				InitializeComponent();
 	}
 }
